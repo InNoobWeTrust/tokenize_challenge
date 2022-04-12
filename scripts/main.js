b1a75d17bcbf0512fba589bb2d0f6e8d35b71846
InNoobWeTrust <vu.truong.16193@gmail.com>
2022-04-13 00:41:37 +0700

    feat: hacking UI
    Finish part 1

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+import Card from './view.js';
+
+fetch('/.netlify/functions/server/market/10')
+  .then(async (res) => {
+    const {bids, asks} = await res.json();
+    const bidElem = document.getElementById('bids');
+    const askElem = document.getElementById('asks');
+
+    bidElem.innerHTML = Card(true, bids);
+    askElem.innerHTML = Card(false, asks);
+  });
