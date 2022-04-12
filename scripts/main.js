71ae0cb3233fd61ebbe2cf7538d38e7102aa2ca5
InNoobWeTrust <forbidden.gun@gmail.com>
2022-04-13 06:44:10 +0700

    feat: periodic update

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,15 @@
 import Card from './view.js';
 
-fetch('/.netlify/functions/server/market/10')
-  .then(async (res) => {
-    const {bids, asks} = await res.json();
-    const bidElem = document.getElementById('bids');
-    const askElem = document.getElementById('asks');
+(() => {
+  const updater = () => (fetch('/.netlify/functions/server/market/10')
+    .then(async (res) => {
+      const {bids, asks} = await res.json();
+      const bidElem = document.getElementById('bids');
+      const askElem = document.getElementById('asks');
 
-    bidElem.innerHTML = Card(true, bids);
-    askElem.innerHTML = Card(false, asks);
-  });
+      bidElem.innerHTML = Card(true, bids);
+      askElem.innerHTML = Card(false, asks);
+    }));
+
+  updater().then(() => setInterval(updater, 15000));
+})();
