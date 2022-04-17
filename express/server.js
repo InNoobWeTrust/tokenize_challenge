b6a1cc0d8875f03eee717a9cf87dd8171c265813
InNoobWeTrust <vu.truong.16193@gmail.com>
2022-04-17 12:56:56 +0700

    feat: manual build for front-end

--- express/server.js
+++ express/server.js
@@ -8,6 +8,12 @@ const bodyParser = require('body-parser');
 const getBestBooks = require('./booking');
 const fuzzMarket = require('./fuzz');
 
+const marketFactory = async (numRecords) => {
+  const {bidPrice, askPrice} = await getBestBooks();
+  const fakeMarket = fuzzMarket(bidPrice, askPrice, numRecords || 10);
+  return fakeMarket;
+}
+
 // Router for lambda function
 const router = express.Router();
 router.get('/', (req, res) => {
@@ -17,17 +23,21 @@ router.get('/', (req, res) => {
 });
 router.get('/echo', (req, res) => res.json(req));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
-router.get('/booking', async (req, res) => res.json(await getBestBooks()));
-router.get('/booking', async (req, res) => res.json(await getBestBooks()));
+router.get('/booking', async (_req, res) => res.json(await getBestBooks()));
+
+router.get('/market', async (_req, res) => {
+  const fakeMarket = await marketFactory();
+  res.json(fakeMarket);
+})
 router.get('/market/:num', async (req, res) => {
-  const {bidPrice, askPrice} = await getBestBooks();
-  const fakeMarket = fuzzMarket(bidPrice, askPrice, req.params.num || 10);
+  const fakeMarket = await marketFactory(req.params.num);
   res.json(fakeMarket);
 })
 
 app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+app.use('/.netlify/functions/server', router);
+app.use('/', (_req, res) => res.sendFile(path.join(__dirname, '../static/index.html')));
+app.use('', express.static(path.join(__dirname, 'static')));
 
 module.exports = app;
 module.exports.handler = serverless(app);
